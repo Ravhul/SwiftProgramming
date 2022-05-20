3c7a7b4c7d3e1a73dbe8079680ac8ce95902319f
Rahul Singh <rahulsingh@Rahuls-MacBook-Pro.local>
2022-05-20 07:11:47 +0530

    Selection Sort file added

---
+++ SelectionSort.swift
@@ -0,0 +1,19 @@
+//Selection Sort Program
+//Program is currently under Construction! 
+
+import Foundation
+
+print("Enter the length of the array:");
+
+var len:Int = 0;
+
+if let input = readLine(){
+    if let num = Int(input){
+        len = num;
+    }
+    else{
+        print("Invalid Number!");
+    }
+}
+
+print("Enter \(len) elements of unsorted array:");
